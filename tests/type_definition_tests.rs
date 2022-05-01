40d9bca0d79692c705bf6b2d2520b79cac5e1c3b
Jeroen Vannevel <jer_vannevel@outlook.com>
2022-05-01 22:20:10 +0100

    more tests

--- tests/type_definition_tests.rs
+++ tests/type_definition_tests.rs
@@ -71,17 +71,34 @@ mod tests {
     }
 
     #[test]
-    fn type_definition_add_field_same_child_with_grandkids() {
+    fn type_definition_add_field_same_child_with_kids_added_onto_child() {
         let mut root = TypeDefinition::new("mytype".into(), None);
-        root.add_field(&mut TypeDefinition::new("childprop".into(), None));
+        let mut child = TypeDefinition::new("childprop".into(), None);
 
-        root.add_field(&mut TypeDefinition {
-            name: "childprop".into(),
-            ts_type: TypeDef::NestedType(vec![TypeDefinition::new("grandkiddie".into(), None)]),
-        });
+        child.add_field(&mut TypeDefinition::new("grandkiddie".into(), None));
+        root.add_field(&mut child);
 
         let children = get_children(&root);
         assert_eq!(1, children.len());
         assert_eq!(1, get_children(&children[0]).len());
+    }
+
+    #[test]
+    fn type_definition_add_field_same_child_with_grandkids() {
+        let mut root = TypeDefinition::new("mytype".into(), None);
+        let mut child1 = TypeDefinition::new("childprop".into(), None);
+        let mut child2 = TypeDefinition::new("childprop2".into(), None);
+
+        child1.add_field(&mut TypeDefinition::new("grandkiddie".into(), None));
+        child1.add_field(&mut TypeDefinition::new("grandkiddie2".into(), None));
+        child1.add_field(&mut TypeDefinition::new("grandkiddie3".into(), None));
+        child2.add_field(&mut TypeDefinition::new("grandkiddie".into(), None));
+        root.add_field(&mut child1);
+        root.add_field(&mut child2);
+
+        let children = get_children(&root);
+        assert_eq!(2, children.len());
+        assert_eq!(3, get_children(&children[0]).len());
+        assert_eq!(1, get_children(&children[1]).len());
     }
 }
