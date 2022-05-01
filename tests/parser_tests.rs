fa460bdfcd7cf872f406827b88978bb4385a9308
Jeroen Vannevel <jer_vannevel@outlook.com>
2022-05-01 13:41:25 +0100

    nicer assertions

--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -7,6 +7,7 @@ fn init() {
 
 mod tests {
     use type_rs::parser::add_types;
+    use pretty_assertions::assert_eq;
 
     fn compare(input: &str, expected_output: &str) {
         let output = add_types(String::from(input));
